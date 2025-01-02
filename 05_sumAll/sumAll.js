8eb8b1112dbe15f845ecfc8d0631f935efba4cd2
zelouchan <bad1dje@gmail.com>
2025-01-02 07:12:57 +0000

    Solved 1 to 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -9,15 +9,14 @@ const sumAll = function(num1, num2 ) {
         if ((!Number.isInteger(num1)) || (!Number.isInteger(num2))) 
             return error;
             
-            // if ((typeof num2 !== "number") || (typeof num1 !== "number")) 
-            //     return error;
+            if ((typeof num2 !== "number") || (typeof num1 !== "number")) 
+                return error;
 
 // Sorts the highest and lowest number
         let big = Math.max(num1, num2);
         let small = Math.min(num1, num2);
    
 // actual loop that does the calculations
-               
                     for (let i = small; i <= big; i++)
                     {sum += i;} return sum;    
 
