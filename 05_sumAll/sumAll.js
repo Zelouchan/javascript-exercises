f474939068e0db13649ab8c5b199067422ef95c3
zelouchan <bad1dje@gmail.com>
2025-01-02 07:08:23 +0000

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,19 +2,24 @@ const sumAll = function(num1, num2 ) {
     let sum = 0;
     let error = 'ERROR';
 
-// Sorts the highest and lowest number
-    let big = Math.max(num1, num2);
-    let small = Math.min(num1, num2);
-
 // Check for problems with input
-    if ((big <= 0) || (small <= 0)) return error;
-    if ((!Number.isInteger(small)) || (!Number.isInteger(big))) return error;
-    if ((typeof big !== "number") || (typeof small !== "number")) return error;
+    if ((num2 <= 0) || (num1 <= 0)) 
+        return error;
+        
+        if ((!Number.isInteger(num1)) || (!Number.isInteger(num2))) 
+            return error;
+            
+            // if ((typeof num2 !== "number") || (typeof num1 !== "number")) 
+            //     return error;
+
+// Sorts the highest and lowest number
+        let big = Math.max(num1, num2);
+        let small = Math.min(num1, num2);
    
 // actual loop that does the calculations
-    else  if ((big <= 0) || (small <= 0)) return error; 
-            for (let i = small; i <= big; i++)
-        {sum += i} return sum;    
+               
+                    for (let i = small; i <= big; i++)
+                    {sum += i;} return sum;    
 
 };
 
